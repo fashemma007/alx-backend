a010530d206853d18d34c800739956c105eacde4
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-05-23 13:08:08 +0100

    feat[task-2]: Create a get_locale function with the babel.localeselector decorator

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """module docs for 1-app.py"""
-from flask import Flask, render_template
-from flask_babel import Babel
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
 
 
 class Config(object):
@@ -13,14 +13,21 @@ class Config(object):
 
 app = Flask(__name__)
 app.config.from_object(Config)  # load configurations
-# print(app.config)  # see all configs
+print(app.config)  # see all configs
 babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """get locale"""
+    return 'fr'
+    # return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route("/")
 def index():
     """Homepage"""
-    results = {"Hello world"}
+    results = "Hello world"
     return render_template("0-index.html", results=results)
 
 
